6eecd58dcc685b7916fc72a9217111e1517826af
muxrwc <muxrwc@126.com>
2017-09-22 12:02:09 +0800

    add: peg build

--- nofile.js
+++ nofile.js
@@ -1,15 +1,24 @@
 let kit = require('nokit')
 
 module.exports = function (task) {
+    function pegBuild() {
+        return kit.spawn('pegjs', ['-o', 'src/grammar/parser.js', 'src/grammar/lang.pegjs'])
+    }
     task('default', function () {
-        return kit.spawn('tsc', ['-w'])
+        return pegBuild().then(function () {
+            return kit.spawn('tsc', ['-w'])
+        })
     })
 
     task('build', function () {
-        return kit.spawn('tsc')
+        return pegBuild().then(function () {
+            return kit.spawn('tsc')
+        })
     })
 
     task('test', function () {
-        return kit.spawn('junit', ['test/*.js'])
+        return pegBuild().then(function () {
+            return kit.spawn('junit', ['test/*.js'])
+        })
     })
 }
