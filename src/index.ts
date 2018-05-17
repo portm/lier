df5ea2572e5bd7bb1e87647d13ede38b9f6cfbe9
muxrwc <muxrwc@126.com>
2018-05-17 14:05:06 +0800

    opt: validate declare arg

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,28 @@ function assert (data, type, declares?) {
     }
 }
 
-function assertx (data, lang, declares = {}) {
-    const tree = compile(parse(lang))
-    return assert(data, tree.assignment, _.defaultsDeep(declares, tree.declares))
+function defaultDeps (declares, defaultsDeclares = {}) {
+    const deps = {}
+    for (const key of Object.keys(defaultsDeclares)) {
+        const tree = compile(parse(defaultsDeclares[key]))
+        deps[key] = tree.assignment
+    }
+    return _.defaultsDeep(deps, declares)
 }
 
-function validatex (data, lang, declares = {}) {
+function assertx (data, lang, declares?) {
     const tree = compile(parse(lang))
-    return validate(data, tree.assignment, _.defaultsDeep(declares, tree.declares))
+    return assert(data, tree.assignment, defaultDeps(tree.declares, declares))
 }
 
-function mockx (lang, declares = {}) {
+function validatex (data, lang, declares?) {
     const tree = compile(parse(lang))
-    return mock(tree.assignment, _.defaultsDeep(declares, tree.declares))
+    return validate(data, tree.assignment, defaultDeps(tree.declares, declares))
+}
+
+function mockx (lang, declares?) {
+    const tree = compile(parse(lang))
+    return mock(tree.assignment, defaultDeps(tree.declares, declares))
 }
 
 export {
